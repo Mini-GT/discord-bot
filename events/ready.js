a4069443c18daea3b061c0ed9f2f9ebd64b27732
MiniGT <bendanillo539@gmail.com>
2024-10-07 22:10:23 +0800

    added bot rich presence `watching` base on how many members is in the server

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,36 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
+const { guildId } = require('../config.json');
+
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		// console.log(client.guilds.cache.get("guildId"))
+		// const {memberCount, name } = client.guilds.cache.get("guildId");
+		const guild = client.guilds.cache.get(guildId);
+		updateMemberCount(guild)
+		
+		// Event listener for when a member joins/leaves the guild
+			const handleMemberChange = member => {
+				const guild = member.guild;
+				updateMemberCount(guild);
+		};
+
+		client.on('guildMemberAdd', handleMemberChange);
+		client.on('guildMemberRemove', handleMemberChange);
+
+		function updateMemberCount(guild) {
+			if (guild) {
+					const { name, memberCount} = guild;
+					client.user.setActivity(`${name}: ${memberCount} members`, { type: ActivityType.Watching });
+			} else {
+					console.error("Guild not found.");
+			}
+		};
 	},
 };
+
+
+
